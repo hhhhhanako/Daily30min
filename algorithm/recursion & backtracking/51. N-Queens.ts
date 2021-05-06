86823e89770dbc36220da390fcbce4fe4873e386
hhhhhanako <417978248@qq.com>
2021-05-07 00:09:39 +0800

    feat(dfs): finish n-queen problem

--- algorithm/recursion & backtracking/51. N-Queens.ts
+++ algorithm/recursion & backtracking/51. N-Queens.ts
@@ -3,24 +3,32 @@
 
 function solveNQueens(n: number): string[][] {
   let res = []
-  let initVal = new Array(n).fill('.')
-  let board = new Array(n).fill(initVal)
-  let check = (x,y) => {
-    for(let i=0; i< n; i++) {
-      for(let j=0;j<n;j++) {
-        if(board[i][j] === 'Q' && (j==y || x+y == i+j || x-y == i-j )) return true
+  let board = new Array(n)
+  for (let i = 0; i < n; i++) {
+    board[i] = new Array(n).fill('.')
+  }
+
+  let check = (x, y) => {
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (board[i][j] === 'Q' && (j == y || x + y == i + j || x - y == i - j))
+          return true
       }
     }
     return false
   }
-  let dfs = (arr, index) => {
-    if(index === n) {
-      // let 
+  let dfs = (index) => {
+    if (index === n) {
+      let resArr = board.map((item) => item.join(''))
+      return res.push(resArr)
     }
-    for(let i = 0;i<n;i++) {
-
+    for (let i = 0; i < n; i++) {
+      if (check(index, i)) continue
+      board[index][i] = 'Q'
+      dfs(index + 1)
+      board[index][i] = '.'
     }
   }
-  dfs(board,0)
+  dfs(0)
   return res
-};
+}
