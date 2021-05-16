410d5e4f7e781d89a1aa661c571c30c62daf5314
hhhhhanako <417978248@qq.com>
2021-05-16 23:56:20 +0800

    feat(binaryTree): update the solution of Problem 129

--- algorithm/binary tree/129. Sum Root to Leaf Numbers.ts
+++ algorithm/binary tree/129. Sum Root to Leaf Numbers.ts
@@ -1,14 +1,29 @@
+// function sumNumbers(root: TreeNode | null): number {
+//   let sum = 0
+//   let dfs = (numStr, node) => {
+//     if (!node) return
+//     const newStr = numStr + node.val
+//     if (!node.left && !node.right) {
+//       return sum += Number(newStr)
+//     }
+//     if (node.left) dfs(newStr, node.left)
+//     if (node.right) dfs(newStr, node.right)
+//   }
+//   dfs('', root)
+//   return sum
+// };
+
 function sumNumbers(root: TreeNode | null): number {
   let sum = 0
-  let dfs = (numStr, node) => {
+  let dfs = (num, node) => {
     if (!node) return
-    const newStr = numStr + node.val
+    const newNum = num * 10 + node.val
     if (!node.left && !node.right) {
-      return sum += Number(newStr)
+      return sum += newNum
     }
-    if (node.left) dfs(newStr, node.left)
-    if (node.right) dfs(newStr, node.right)
+    if (node.left) dfs(newNum, node.left)
+    if (node.right) dfs(newNum, node.right)
   }
-  dfs('', root)
+  dfs(0, root)
   return sum
 };
