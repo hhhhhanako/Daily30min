c25c85f1ea40e2536638a334a3e3ddf85155025f
hhhhhanako <417978248@qq.com>
2021-06-09 01:49:58 +0800

    feat(): add Function.prototype.bind method

--- ESNEXT/手写方法/call+apply+bind.js
+++ ESNEXT/手写方法/call+apply+bind.js
@@ -4,20 +4,35 @@
 // 3、删除该属性
 
 // Function.call = (thisArgs, arg1, arg2...){}
-const _call = (...args) => {
-  let [content, ...other] = args
-  content = content? Object(content): window
+Function.prototype.call = function (thisArgs, ...args) {
+  thisArgs = thisArgs || window
   const _symbol = Symbol('key')
   content[_symbol] = this
-  const res = content[_symbol](...other)
+  const res = content[_symbol](...args)
   delete content[_symbol]
   return res
 }
 
-const _apply = (...args) => {
-  let [content, ...other] = args
-  content = content? Object(content) : window
+Function.prototype.apply = function (thisArgs, args) {
+  thisArgs = thisArgs || window
   const _symbol = Symbol('key')
   content[_symbol] = this
-  let res
-} 
+  const res = content[_symbol](...args)
+  delete content[_symbol]
+  return res
+}
+
+// 对于普通函数，绑定this指向
+// 对于构造函数，要保证原函数的原型对象上的属性不能丢失
+Function.prototype.bind = function (thisArgs, ...args) {
+  let self = this
+  let res = function () {
+    return self.call(
+      this instanceof self ? this : thisArgs,
+      ...args,
+      ...arguments
+    )
+  }
+  res.prototype = Object.create(self.prototype)
+  return res
+}
