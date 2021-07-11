295d6c80d712157528fc293da6ea4bfb8a7e6e21
hhhhhanako <417978248@qq.com>
2021-07-11 16:30:10 +0800

    feat(debouce): update debounce method with immediate feature

--- ESNEXT/手写方法/debouce+throttle.js
+++ ESNEXT/手写方法/debouce+throttle.js
@@ -2,25 +2,45 @@
 // 高频事件fn在时间time秒后执行一次，如果time秒内再次触发fn，则重新开始计时
 function myDebouce(fn, time) {
   let timer
-  return function(...args){
+  return function (...args) {
     clearTimeout(timer)
     timer = setTimeout(() => {
       fn.apply(this, args)
-    }, time);
+    }, time)
+  }
+}
+// 立即执行版的防抖函数
+// 立即执行fn，如果time秒内再次触发fn，则重新开始计时
+function myImmediateDebounce(fn, time, immediate) {
+  let timer
+  return function () {
+    if (immediate) {
+      clearTimeout(timer)
+      const startNow = !timer
+      timer = setTimeout(() => {
+        timer = null
+      }, time)
+      if (startNow) {
+        fn.apply(this, arguments)
+      }
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, arguments)
+      }, time)
+    }
   }
 }
 
 // 节流
 // 高频事件fn在time内只执行一次
-function myThrottle(fn, time){
+function myThrottle(fn, time) {
   let timer
-  return function(...args) {
-    if(!timer) {
-      setTimeout(() => {
+  return function (...args) {
+    if (!timer) {
+      timer = setTimeout(() => {
         timer = null
         fn.apply(this, args)
-      }, time);
+      }, time)
     }
   }
-
 }
